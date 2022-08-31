f22f4e06192752e4c0bfff600ad21eefcc12d7d2
Niklas Heidloff <niklas_heidloff@de.ibm.com>
2022-08-31 11:29:02 +0200

    initial commit

---
+++ main.tf
@@ -0,0 +1,76 @@
+locals {
+  name          = "helm-guestbook"
+  bin_dir       = module.setup_clis.bin_dir
+  yaml_dir      = "${path.cwd}/.tmp/${local.name}/chart/${local.name}"
+  #yaml_dir      = "${path.cwd}/.tmp/${local.name}"
+  service_url   = "http://${local.name}.${var.namespace}"
+  cluster_type = var.cluster_type == "kubernetes" ? "kubernetes" : "openshift"
+  values_content = {
+    helm-guestbook = {
+    clusterType = local.cluster_type
+    ingressSubdomain = var.cluster_ingress_hostname
+    "sso.enabled" = var.enable_sso
+    tlsSecretName = var.tls_secret_name
+    replicaCount = "1"
+    "image.repository" = "gcr.io/heptio-images/ks-guestbook-demo"
+    "image.tag" = "0.1"
+    "image.pullPolicy" = "IfNotPresent"
+    "service.type" = "ClusterIP"
+    "service.port" = "80"
+    }
+  }
+  layer = "services"
+  type  = "base"
+  application_branch = "main"
+  namespace = var.namespace
+  layer_config = var.gitops_config[local.layer]
+}
+
+module setup_clis {
+  source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
+}
+
+resource null_resource create_yaml {
+  provisioner "local-exec" {
+    command = "${path.module}/scripts/create-yaml.sh '${local.name}' '${local.yaml_dir}'"
+
+    environment = {
+      VALUES_CONTENT = yamlencode(local.values_content)
+    }
+  }
+}
+
+resource null_resource setup_gitops {
+  depends_on = [null_resource.create_yaml]
+
+  triggers = {
+    name = local.name
+    namespace = var.namespace
+    yaml_dir = local.yaml_dir
+    server_name = var.server_name
+    layer = local.layer
+    type = local.type
+    git_credentials = yamlencode(var.git_credentials)
+    gitops_config   = yamlencode(var.gitops_config)
+    bin_dir = local.bin_dir
+  }
+
+  provisioner "local-exec" {
+    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type '${self.triggers.type}'"
+
+    environment = {
+      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
+      GITOPS_CONFIG   = self.triggers.gitops_config
+    }
+  }
+
+  provisioner "local-exec" {
+    when = destroy
+    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --delete --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type '${self.triggers.type}'"
+
+    environment = {
+      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
+      GITOPS_CONFIG   = self.triggers.gitops_config
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,103 @@
+
+variable "gitops_config" {
+  type        = object({
+    boostrap = object({
+      argocd-config = object({
+        project = string
+        repo = string
+        url = string
+        path = string
+      })
+    })
+    infrastructure = object({
+      argocd-config = object({
+        project = string
+        repo = string
+        url = string
+        path = string
+      })
+      payload = object({
+        repo = string
+        url = string
+        path = string
+      })
+    })
+    services = object({
+      argocd-config = object({
+        project = string
+        repo = string
+        url = string
+        path = string
+      })
+      payload = object({
+        repo = string
+        url = string
+        path = string
+      })
+    })
+    applications = object({
+      argocd-config = object({
+        project = string
+        repo = string
+        url = string
+        path = string
+      })
+      payload = object({
+        repo = string
+        url = string
+        path = string
+      })
+    })
+  })
+  description = "Config information regarding the gitops repo structure"
+}
+
+variable "git_credentials" {
+  type = list(object({
+    repo = string
+    url = string
+    username = string
+    token = string
+  }))
+  description = "The credentials for the gitops repo(s)"
+  sensitive   = true
+}
+
+variable "namespace" {
+  type        = string
+  description = "The namespace where the application should be deployed"
+}
+
+variable "kubeseal_cert" {
+  type        = string
+  description = "The certificate/public key used to encrypt the sealed secrets"
+  default     = ""
+}
+
+variable "server_name" {
+  type        = string
+  description = "The name of the server"
+  default     = "default"
+}
+
+variable "enable_sso" {
+  type        = bool
+  description = "Flag indicating if oauth should be applied (only available for OpenShift)"
+  default     = true
+}
+
+variable "tls_secret_name" {
+  description = "The name of the secret containing the tls certificate values"
+  default     = ""
+}
+
+variable "cluster_ingress_hostname" {
+  type        = string
+  description = "Ingress hostname of the cluster."
+  default     = ""
+}
+
+variable "cluster_type" {
+  description = "The cluster type (openshift or kubernetes)"
+  default     = "openshift"
+}
